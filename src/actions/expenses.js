6ce925da3319ab9a08cebc5eeb3e93a76fdc72f6
c <Christian.lehnert04@icloud.com>
2018-12-03 22:52:50 -0800

    adding new files

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -6,8 +6,8 @@ export const addExpense = (
     description = '',
     note = '',
     amount = 0,
-    createdAt = 0
-  } = {}
+    createdAt = 0,
+  } = {},
 ) => ({
   type: 'ADD_EXPENSE',
   expense: {
@@ -15,19 +15,19 @@ export const addExpense = (
     description,
     note,
     amount,
-    createdAt
-  }
+    createdAt,
+  },
 });
 
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
-  id
+  id,
 });
 
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
   id,
-  updates
+  updates,
 });
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
+import 'react-dates/lib/css/_datepicker.css';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
