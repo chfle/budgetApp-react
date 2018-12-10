7831c93d516329bef026fc4244c2dcf9e0483cd0
c <Christian.lehnert04@icloud.com>
2018-12-09 22:54:06 -0800

    Adding new tests

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: process.env.FIRE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_DATABASE_URL,
+  messagingSenderId: process.env.FIREBASE_DATABASE_URL,
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { database, firebase };
