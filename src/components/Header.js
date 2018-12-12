83dfa02e6c88bd02a2aab8e06d9310c3bea57bc0
c <Christian.lehnert04@icloud.com>
2018-12-12 03:44:41 -0800

    styling login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
-  <header>
-    <h1>Budget App</h1>
-    <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <button onClick={startLogout}>logout</button>
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <Link className="header__title" to="/dashboard">
+          <h1>Budget</h1>
+        </Link>
+        <button onClick={startLogout}>logout</button>
+      </div>
+    </div>
   </header>
 );
 
@@ -17,4 +21,3 @@ const MapDispatchToProps = dispatch => ({
 });
 
 export default connect(undefined, MapDispatchToProps)(Header);
-
